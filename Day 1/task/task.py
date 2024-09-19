2d9ff8d4eb0b84145393b05c9a209889823b870b
Clayton Watkins <watkinclayton17@yahoo.com>
2024-09-18 17:00:47 -0700

    finished day 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello! In order to make a name for you band, you'll need to answer a few questions.")
+city = input("What city did you grow up in?")
+petName = input("What is a name of a pet you grew up with?")
+print("Your band name could be: " + city + " " + petName + ".")
